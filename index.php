77b4443c5fa8ada51e466147f99cc8490ab9f5fb
Suggs <mikesuggitt@gmail.com>
2013-04-28 23:41:11 +0100

    Added pages

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
 <html>
     <head>
+        <title>Mike Suggitt</title>
         <link rel="stylesheet" href="lib/css/core.css"/>
     </head>
     <body>
-        <header>
+        <header id="top">
             <h1>Mike Suggitt</h1>
             <nav>
                 <ul>
-                    <li><a href="">about</a></li>
-                    <li><a href="">work</a></li>
-                    <li><a href="">contact</a></li>
+                    <li><a href="#about">about</a></li>
+                    <li><a href="#work">work</a></li>
+                    <li><a href="#contact">contact</a></li>
                 </ul>
             </nav>
         </header>
 
 
+        <article id="about">
+            <h2>About<span class="back-to-top"><a href="#top">top</a></span></h2>
+        </article>
+
+        <article id="work">
+            <h2>Work<span class="back-to-top"><a href="#top">top</a></span></h2>
+        </article>
+
+        <article id="contact">
+            <h2>Contact<span class="back-to-top"><a href="#top">top</a></span></h2>
+        </article>
 
 
     </body>

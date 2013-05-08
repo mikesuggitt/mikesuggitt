304b4ebd35c9f391eaee3af059a757a1d9e57dad
mikesuggitt <Keyboard84>
2013-05-08 17:34:33 +0100

    Added placeholder text

--- index.php
+++ index.php
@@ -13,19 +13,34 @@
                     <li><a href="#contact">contact</a></li>
                 </ul>
             </nav>
+
+            <div class="quote">
+                <script type="text/javascript" src="http://www.brainyquote.com/link/quotebr.js"></script>
+            </div>
         </header>
+
+
 
 
         <article id="about">
             <h2>About<span class="back-to-top"><a href="#top">top</a></span></h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
         </article>
 
         <article id="work">
             <h2>Work<span class="back-to-top"><a href="#top">top</a></span></h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
         </article>
 
         <article id="contact">
             <h2>Contact<span class="back-to-top"><a href="#top">top</a></span></h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
         </article>
 
 

c5fc7fbb5fcb561e256cdb57243566a8cac141bf
mikesuggitt <Keyboard84>
2013-08-14 14:12:41 +0100

    Added bootstrap styleguide

---
+++ index-bootstrap.php
@@ -0,0 +1,77 @@
+
+
+<html>
+    <head>
+        <title>Mike Suggitt</title>
+        <link rel="stylesheet" href="lib/css/core.css"/>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Mike Suggitt</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                </ul>
+            </div>
+        </nav>
+
+
+        <script type="text/javascript" src="lib/js/vendor/jquery.js"></script>
+        <script type="text/javascript" src="lib/js/vendor/bootstrap.js"></script>
+    </body>
+
+<!--    -->
+<!--        <header id="top">-->
+<!--            <h1>Mike Suggitt</h1>-->
+<!--            <nav>-->
+<!--                <ul>-->
+<!--                    <li><a href="#about">about</a></li>-->
+<!--                    <li><a href="#work">work</a></li>-->
+<!--                    <li><a href="#contact">contact</a></li>-->
+<!--                </ul>-->
+<!--            </nav>-->
+<!---->
+<!--            <div class="quote">-->
+<!--                <script type="text/javascript" src="http://www.brainyquote.com/link/quotebr.js"></script>-->
+<!--            </div>-->
+<!--        </header>-->
+<!---->
+<!---->
+<!---->
+<!---->
+<!--        <article id="about">-->
+<!--            <h2>About<span class="back-to-top"><a href="#top">top</a></span></h2>-->
+<!--            <p>-->
+<!--                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.-->
+<!--            </p>-->
+<!--        </article>-->
+<!---->
+<!--        <article id="work">-->
+<!--            <h2>Work<span class="back-to-top"><a href="#top">top</a></span></h2>-->
+<!--            <p>-->
+<!--                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.-->
+<!--            </p>-->
+<!--        </article>-->
+<!---->
+<!--        <article id="contact">-->
+<!--            <h2>Contact<span class="back-to-top"><a href="#top">top</a></span></h2>-->
+<!--            <p>-->
+<!--                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.-->
+<!--            </p>-->
+<!--        </article>-->
+<!---->
+<!---->
+<!--    </body>-->
+</html>
